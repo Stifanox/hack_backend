2e81601bca15f1c78a3e2a9a72d19a8d828393c1
Michał Proć <michalproc4@gmail.com>
2024-01-20 17:14:39 +0100

    Add new models

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -3,4 +3,3 @@ from flask import Blueprint
 bp = Blueprint('api', __name__)
 
 from app.api import users
-from app.api import anonim_messages
--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from app import db
 
 
@@ -26,67 +28,55 @@ class User(db.Model):
             self.set_password(data['password'])
 
 
-class AnonimMessage(db.Model):
+class DailyUpdate(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    receiver_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    message = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    rating = db.Column(db.Integer)
+    note = db.Column(db.Text)
+    is_cheered = db.Column(db.Boolean, default=False)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
     def __repr__(self):
-        return f'<AnonimMessage {self.id}>'
+        return f'<DailyUpdate {self.id}>'
 
     def toDict(self):
         return {
             "id": self.id,
-            "senderId": self.sender_id,
-            "receiverId": self.receiver_id,
-            "message": self.message
+            "user_id": self.user_id,
+            "rating": self.rating,
+            "note": self.note,
+            "is_cheered": self.is_cheered,
+            "timestamp": self.timestamp.isoformat()
         }
 
     def fromDict(self, data):
-        if 'senderId' in data:
-            self.sender_id = data['senderId']
-        if 'receiverId' in data:
-            self.receiver_id = data['receiverId']
-        if 'message' in data:
-            self.message = data['message']
+        for field in ['user_id', 'rating', 'note', 'is_cheered']:
+            if field in data:
+                setattr(self, field, data[field])
 
 
-from datetime import datetime
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-
-class DailyMood(db.Model):
+class Cheerup(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    short_description = db.Column(db.String(120), nullable=False)
-    description = db.Column(db.Text, nullable=True)
-    mood = db.Column(db.String(50), nullable=False)
-    date = db.Column(db.DateTime, default=datetime.utcnow)
+    update_id = db.Column(db.Integer, db.ForeignKey('daily_update.id'))
+    receiver_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    content = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
     def __repr__(self):
-        return f'<DailyMood {self.id}>'
+        return f'<Cheerup {self.id}>'
 
     def toDict(self):
         return {
             "id": self.id,
-            "userId": self.user_id,
-            "shortDescription": self.short_description,
-            "description": self.description,
-            "mood": self.mood,
-            "date": self.date.isoformat() if self.date else None
+            "update_id": self.update_id,
+            "receiver_id": self.receiver_id,
+            "sender_id": self.sender_id,
+            "content": self.content,
+            "timestamp": self.timestamp.isoformat() if self.timestamp else None
         }
 
     def fromDict(self, data):
-        if 'userId' in data:
-            self.user_id = data['userId']
-        if 'shortDescription' in data:
-            self.short_description = data['shortDescription']
-        if 'description' in data:
-            self.description = data['description']
-        if 'mood' in data:
-            self.mood = data['mood']
-        if 'date' in data:
-            self.date = datetime.fromisoformat(data['date']) if data['date'] else None
+        for field in ['update_id', 'receiver_id', 'sender_id', 'content']:
+            if field in data:
+                setattr(self, field, data[field])
