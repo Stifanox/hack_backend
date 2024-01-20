21df93246a337d17549817097d2ce95bb3a24c0d
Michał Proć <michalproc4@gmail.com>
2024-01-20 15:59:37 +0100

    [WiP] DailyMood

--- app/models.py
+++ app/models.py
@@ -50,3 +50,43 @@ class AnonimMessage(db.Model):
             self.receiver_id = data['receiverId']
         if 'message' in data:
             self.message = data['message']
+
+
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class DailyMood(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    short_description = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    mood = db.Column(db.String(50), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<DailyMood {self.id}>'
+
+    def toDict(self):
+        return {
+            "id": self.id,
+            "userId": self.user_id,
+            "shortDescription": self.short_description,
+            "description": self.description,
+            "mood": self.mood,
+            "date": self.date.isoformat() if self.date else None
+        }
+
+    def fromDict(self, data):
+        if 'userId' in data:
+            self.user_id = data['userId']
+        if 'shortDescription' in data:
+            self.short_description = data['shortDescription']
+        if 'description' in data:
+            self.description = data['description']
+        if 'mood' in data:
+            self.mood = data['mood']
+        if 'date' in data:
+            self.date = datetime.fromisoformat(data['date']) if data['date'] else None
