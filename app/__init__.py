a37689076b3d8aed03f18a3668881855a3838bac
Stifanox <mateusz.fudzsl@hotmail.com>
2024-01-20 14:58:05 +0100

    Can get user from link

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,6 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from app.api import bp as api_bp
-
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,6 +9,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from app import routes, models
+from app.api import bp as api_bp
+app.register_blueprint(api_bp)
 
-app.app_context().push()
-app.register_blueprint(api_bp, url_prefix='/api')
+
--- app/api/users.py
+++ app/api/users.py
@@ -2,11 +2,7 @@ from app.api import bp
 from app.models import User
 from flask import jsonify
 
-@bp.route("/users/<int:id>", method=["GET"])
+
+@bp.route("/users/<int:id>", methods=["GET"])
 def getUser(id):
     return jsonify(User.query.get_or_404(id).toDict())
-
-
-@bp.route("/users", method=["POST"])
-def addUser():
-    pass
