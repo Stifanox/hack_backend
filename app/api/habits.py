d7becfe3a9bed4e7911ba3dff920c29e44a4b702
Stifanox <mateusz.fudzsl@hotmail.com>
2024-01-20 18:01:15 +0100

    Added status codes to responses

--- app/api/habits.py
+++ app/api/habits.py
@@ -8,32 +8,36 @@ from app import db
 @bp.route("/habits", methods=["POST"])
 def createNewHabit():
     if request.content_type != "application/json":
-        return ErrorHabit("Content type is not application/json").__dict__
+        return ErrorHabit("Content type is not application/json").__dict__, 404
 
     if "habitId" not in request.json or "userId" not in request.json:
-        return ErrorHabit("Not all required params was passed (userId, habitId)").__dict__
+        return ErrorHabit("Not all required params was passed (userId, habitId)").__dict__, 404
 
     newHabit = Habits(user_id=request.json["userId"], habit_id=request.json["habitId"])
 
     db.session.add(newHabit)
     db.session.commit()
 
-    return SuccessHabit("Habit was added successfully").__dict__
+    return SuccessHabit("Habit was added successfully").__dict__,200
 
 
 @bp.route("/habits", methods=["PUT"])
 def updateHabit():
     if request.content_type != "application/json":
-        return ErrorHabit("Content type is not application/json").__dict__
+        return ErrorHabit("Content type is not application/json").__dict__, 404
 
     if "id" not in request.json or "broken" not in request.json:
-        return ErrorHabit("Not all required params was passed (id, broken)").__dict__
+        return ErrorHabit("Not all required params was passed (id, broken)").__dict__, 404
 
     habit = Habits.query.filter_by(id=request.json["id"]).first()
 
     if not habit:
-        return ErrorHabit("Habit was not found").__dict__
+        return ErrorHabit("Habit was not found").__dict__, 404
 
     habit.broken = request.json["broken"]
     db.session.commit()
-    return SuccessHabit("Habit was successfully updated").__dict__
+    return SuccessHabit("Habit was successfully updated").__dict__, 200
+
+@bp.route("/habits/<int:id>", methods=["GET"])
+def getUserHabits():
+    pass
