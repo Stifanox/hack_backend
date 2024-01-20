d246faa107ca21b52ca939080d49ae1beaefbd32
Stifanox <mateusz.fudzsl@hotmail.com>
2024-01-20 16:30:57 +0100

    Changed the form data to application json content type

--- app/api/users.py
+++ app/api/users.py
@@ -13,31 +13,31 @@ def getUser(id):
 
 @bp.route("/login", methods=["POST"])
 def loginUser():
-    if "user" not in request.form or "password" not in request.form:
+    if "user" not in request.json or "password" not in request.json:
         return ErrorLogin("There are no required fields ('user', 'password')").__dict__
 
-    user = User.query.filter_by(username=request.form["user"]).first()
+    user = User.query.filter_by(username=request.json["user"]).first()
 
     if not user:
         return ErrorLogin("Invalid username or password").__dict__
 
-    if user.password == request.form["password"]:
-        return SuccessLogin({"user": request.form["user"]}).__dict__
+    if user.password == request.json["password"]:
+        return SuccessLogin({"user": request.json["user"]}).__dict__
     else:
         return ErrorLogin("Invalid username or password").__dict__
 
 
 @bp.route("/register", methods=["POST"])
 def registerUser():
-    if "user" not in request.form or "password" not in request.form:
+    if "user" not in request.json or "password" not in request.json:
         return ErrorRegister("There are no required fields ('user', 'password')").__dict__
 
-    checkUser = User.query.filter_by(username=request.form["user"]).first()
+    checkUser = User.query.filter_by(username=request.json["user"]).first()
 
     if checkUser:
         return ErrorRegister("User exists").__dict__
 
-    newUser = User(username=request.form["user"], password=request.form["password"])
+    newUser = User(username=request.json["user"], password=request.json["password"])
     db.session.add(newUser)
     db.session.commit()
 
